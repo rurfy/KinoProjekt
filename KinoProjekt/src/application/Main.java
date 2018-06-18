6b0641b026e8171861922fe0d010e8910659b141
ChristopherChRichter <ChristopherChRichter@BK7N00105290.bku.db.de>
2018-06-18 12:30:14 +0200

    FilmInfo erstellt

--- KinoProjekt/src/application/Main.java
+++ KinoProjekt/src/application/Main.java
@@ -1,19 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private final String STARTBILDSCHIRM = "StartBildschirm.fxml";
+	private final String FILMINFO = "FilmInfo.fxml";
+	private final String SITZPLATZAUSWAHL = "SitzplatzAuswahl.fxml";
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root1 = FXMLLoader.load(getClass().getResource(STARTBILDSCHIRM));
+			Scene startBildschirm = new Scene(root1);
+			Parent root2 = FXMLLoader.load(getClass().getResource(FILMINFO));
+			Scene filmInfo = new Scene(root2);
+			Parent root3 = FXMLLoader.load(getClass().getResource(SITZPLATZAUSWAHL));
+			Scene sitzplatzAuswahl = new Scene(root3);
+			startBildschirm.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(filmInfo);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
