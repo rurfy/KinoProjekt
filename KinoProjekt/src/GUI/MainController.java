3d0ab8c93c725bc8c52b6ff117b59ffe1d7f204d
ChristopherChRichter <ChristopherChRichter@BK7N00105290.bku.db.de>
2018-07-09 20:11:03 +0200

    Design und GUI Update

---
+++ KinoProjekt/src/GUI/MainController.java
@@ -0,0 +1,44 @@
+package GUI;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public abstract class MainController implements Initializable{
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public abstract void zurSitzplatzAuswahl(ActionEvent e);
+	
+	public abstract void zurFilmInfo(ActionEvent e);
+	
+	public abstract void zumStartBildschirm(ActionEvent e);
+	
+	public abstract void zurFilmInfo(MouseEvent e);
+	
+	public void setNewScene(String fxml, Control c) {
+		// TODO Auto-generated method stub
+		Stage primaryStage = (Stage) c.getScene().getWindow();
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource(fxml));
+			root.getStylesheets().add(getClass().getResource("design.css").toExternalForm());
+			primaryStage.setScene(new Scene(root));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
