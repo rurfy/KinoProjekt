b8ac481e2cfe33ab3e6731f402bc733966fa5465
LucasMiekley <LucasMiekley@MDXN00020760.bku.db.de>
2018-08-12 13:53:47 +0200

    automatisches erzeugen der Sitzplätze

--- KinoProjekt/src/GUI/ControllerSitzplatzauswahl.java
+++ KinoProjekt/src/GUI/ControllerSitzplatzauswahl.java
@@ -3,45 +3,48 @@ package GUI;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.sun.prism.paint.Color;
+
+import Platztypen.Sitzplatz;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class ControllerSitzplatzauswahl extends MainController implements Initializable{
-
+public class ControllerSitzplatzauswahl extends MainController implements Initializable {
 
 	private final String STARTBILDSCHIRM = "StartBildschirm.fxml";
 	private final String FILMINFO = "FilmInfo.fxml";
-	
+
 	@FXML
 	private Button sitzPlatzZurueck;
-	
+
 	@FXML
 	private Pane sitzplaetze;
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@FXML
 	public void zurSitzplatzAuswahl(ActionEvent e) {
-		//setNewScene(SITZPLATZAUSWAHL, filmInfoZurueck);
+		// setNewScene(SITZPLATZAUSWAHL, filmInfoZurueck);
 	}
-
 
 //	@Override
 //	public void zurFilmInfo(ActionEvent e) {
 //		// TODO Auto-generated method stub
 //		setNewScene(FILMINFO, sitzPlatzZurueck);
 //	}
-
 
 	@Override
 	public void zumStartBildschirm(ActionEvent e) {
@@ -54,30 +57,57 @@ public class ControllerSitzplatzauswahl extends MainController implements Initia
 		// TODO Auto-generated method stub
 		setNewScene(FILMINFO, sitzPlatzZurueck);
 	}
-	
+
 	public void initData() {
-		generiereSitzplaetze(10, 10);
+		generiereSitzplaetze(12, 22);
 	}
-	
+
 	public void generiereSitzplaetze(int reihe, int spalte) {
-		Button[][] platz = new Button[reihe][spalte];
+		Sitzplatz platz = new Sitzplatz();
 		Stage primaryStage = (Stage) sitzPlatzZurueck.getScene().getWindow();
+//		int abstand = 30;			
+
 		
-		
-		for(int i = 0; i<reihe; i++ ) {
-			for(int j = 0; j<spalte; j++) {
+		for (int i = 0; i < reihe; i++) {
 			
-				platz[i][j] = new Button();
-				platz[i][j].setPrefSize(30,30);
-				platz[i][j].setMinSize(30,30);
-				platz[i][j].setMaxSize(30, 30);
-				platz[i][j].setLayoutY(100+i*35);
-				platz[i][j].setLayoutX(100+j*35);
-				sitzplaetze.getChildren().add(platz[i][j]);
+//			if (i == 4) {
+//				abstand = abstand + 25;
+//			} else if (i == 8) {
+//				abstand = abstand + 30;
+//			}
+			for (int j = 0; j < spalte; j++) {
+				System.out.println(i+" "+j);
+				platz.erstelleSitzplatz(i,j, primaryStage);
+//				platz[i][j] = new Button();
+//				platz[i][j].setPrefSize(25, 25);
+//				platz[i][j].setMinSize(25, 25);
+//				platz[i][j].setMaxSize(25, 25);
+
+//				platz[i][j].setLayoutY(52 + i * 30 + abstand);
+//				platz[i][j].setLayoutX(130 + j * 30);
+//				platz[i][j].setId("Reihe: "+i+", Platz: "+j);
+//				platz[i][j].getStyleClass().removeAll("button");
+//				platz[i][j].getStyleClass().add("onClick");
+//				platz[i][j].setOnAction(new EventHandler<ActionEvent>(){
+//					
+//					@Override
+//					public void handle(ActionEvent e) {
+//						Button x = (Button) e.getSource();
+//						if(x.getStyleClass().get(0)=="clicked") {
+//							x.getStyleClass().removeAll("clicked");
+//							x.getStyleClass().add("onClick");
+//						}else {
+//							x.getStyleClass().removeAll("onClick");
+//							x.getStyleClass().add("clicked");
+//						}
+//					}
+//					
+//				});
+//				sitzplaetze.getChildren().add(platz[i][j]);
 			}
-			
+
 		}
-		primaryStage.show();
+//		primaryStage.show();
 	}
-	
+
 }
--- KinoProjekt/src/Platztypen/Sitzplatz.java
+++ KinoProjekt/src/Platztypen/Sitzplatz.java
@@ -1,19 +1,68 @@
 package Platztypen;
 
-public abstract class Sitzplatz {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Sitzplatz {
 	private boolean belegt;
-	private int platzID;
+	private String platzID;
+//platzID wurde zu String geändert, aber in modell steht es als int drin !!!
+	@FXML
+	private Pane sitzplaetze;
 	
 	public boolean isBelegt() {
 		return belegt;
 	}
+
 	public void setBelegt(boolean belegt) {
 		this.belegt = belegt;
 	}
-	public int getPlatzID() {
+
+	public String getPlatzID() {
 		return platzID;
 	}
-	public void setPlatzID(int platzID) {
+
+	public void setPlatzID(String platzID) {
 		this.platzID = platzID;
 	}
+
+	public void erstelleSitzplatz(int i, int j, Stage primaryStage) {
+		Button platz = new Button();
+		int abstand = 30;
+		if (i >= 4 && i < 8) {
+			abstand = abstand + 25;
+		} else if (i >= 8) {
+			abstand = abstand + 30;
+		}
+		platz.setPrefSize(25, 25);
+		platz.setLayoutX(130 + j * 30);
+		platz.setLayoutY(52 + i * 30);
+		platz.setId("Reihe: "+i+", Platz: "+j);
+		platz.getStyleClass().removeAll("button");
+		platz.getStyleClass().add("onClick");
+		platzID = platz.getId();
+		platz.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent e) {
+//				Button x = (Button) e.getSource();
+				if(platz.getStyleClass().get(0)=="clicked") {
+					platz.getStyleClass().removeAll("clicked");
+					platz.getStyleClass().add("onClick");
+					belegt=false;
+				}else {
+					platz.getStyleClass().removeAll("onClick");
+					platz.getStyleClass().add("clicked");
+					belegt=true;
+				}
+			}
+		});
+		sitzplaetze.getChildren().add(platz);
+		primaryStage.show();
+	}
+	
 }
