d4cda94cd5bca9ec630523bb74faf298e93c5935
rurfy <37702988+rurfy@users.noreply.github.com>
2018-08-23 15:45:26 +0200

    Mit erster Jar

--- KinoProjekt/src/GUI/Controller4.java
+++ KinoProjekt/src/GUI/Controller4.java
@@ -8,8 +8,6 @@ import Default.Kunde;
 import Default.Reservierung;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
